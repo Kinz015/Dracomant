1f1dc202fe8e9d6b7d02973685347d1c9f6c5325
Guilherme Lins <guilhermelins978@gmail.com>
2025-02-04 18:49:52 -0300

    teste vercel 5

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: 'frontend', // Define a pasta como root
   plugins: [react()],
-  base: '/', // Caminho base no ambiente de produção
-  build: {
-    outDir: 'dist', // Saída para a pasta "dist" na raiz
-    emptyOutDir: true, // Limpa a saída antes de cada build
-  },
 });
