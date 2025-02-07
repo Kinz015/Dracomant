7dc32f504c9cedfb56baa639129e6587dee3f8bc
Guilherme Lins <guilhermelins978@gmail.com>
2025-02-07 19:42:37 -0300

    Coloquei link no nome e na logo

--- frontend/src/Components/Header/LogoENome/index.jsx
+++ frontend/src/Components/Header/LogoENome/index.jsx
@@ -1,13 +1,16 @@
+import { Link } from "react-router-dom";
 import logo from "../../../assets/logo.png";
 import styles from "./index.module.css";
 
 export default function LogoENome() {
   return (
     <div className={styles.iconeENome}>
-      <a href="#">
+      <Link href="#">
         <img className={styles.logo} src={logo} alt="" />
-      </a>
-      <h1 className={styles.titulo}>Dracomant</h1>
+      </Link>
+      <Link href="#">
+        <h1 className={styles.titulo}>Dracomant</h1>
+      </Link>
     </div>
   );
 }
