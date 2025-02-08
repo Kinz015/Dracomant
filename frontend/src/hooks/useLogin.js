948386ad7f22c49c600ea047bdbcd0342a719fb6
Guilherme Lins <guilhermelins978@gmail.com>
2025-02-08 17:20:00 -0300

    Revert "Teste api do vercek"
    This reverts commit 076048c2ca84e391297c1a0f91f8fe76aa328abe.

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -9,7 +9,7 @@ const useLogin = () => {
     setError(null);
 
     try {
-      const response = await fetch("https://dracomant.vercel.app/login", {
+      const response = await fetch("http://localhost:5000/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
