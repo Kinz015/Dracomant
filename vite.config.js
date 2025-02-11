9513177c00e7c7baa4d6b25ba85be60cce13b61f
Guilherme Lins <guilhermelins978@gmail.com>
2025-02-11 19:08:03 -0300

    Teste 2 concertar vercel

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: './', // Define a raiz do projeto como o diretório atual
   plugins: [react()],
-  build: {
-    outDir: './dist', // Define o diretório de saída para a build
-  },
+  base: '/',
 });
