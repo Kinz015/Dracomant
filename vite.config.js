6d02e90136c375b6b33cee3b759a6965d3c2732d
Guilherme Lins <guilhermelins978@gmail.com>
2025-02-11 19:10:50 -0300

    teste 3

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  build: {
+    outDir: 'dist', // Certifique-se de que o diretório de build é 'dist'
+  },
 });
