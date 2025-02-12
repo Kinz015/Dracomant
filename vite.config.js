cc1d3dcafe61deb9859eae9b68f158e790782059
Guilherme Lins <guilhermelins978@gmail.com>
2025-02-12 12:31:13 -0300

    teste de deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Certifique-se de que o diretório de build é 'dist'
+    outDir: '/frontend/dist', // Certifique-se de que o diretório de build é 'dist'
   },
 });
