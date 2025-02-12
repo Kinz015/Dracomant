8d380a9136d6023d89b4422787f401bb02108dfe
Guilherme Lins <guilhermelins978@gmail.com>
2025-02-12 13:56:31 -0300

    teste

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Garante que os caminhos funcionem corretamente
   build: {
     outDir: 'dist' // Garante que o build seja gerado na pasta correta
   }
